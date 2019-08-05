76615cdb057280f2cf579232b0c7ebd2e67b90a1
Eric McBride <eric.mcbride@toyotaconnected.com>
2019-08-05 09:24:26 -0500

    Switched to v5

--- src/parse.rs
+++ src/parse.rs
@@ -1,151 +1,98 @@
+use nom::{sequence::tuple, IResult};
+
 #[derive(Debug)]
 pub struct ScenarioOutline {
     scenario: String,
-    tags: String,
+    tags: Vec<String>,
+    feature: String,
+    given: String,
     when: String,
     then: String,
 }
 
 named!(scenario_outline_parser<&str, &str>,
-    tag_s!("Scenario Outline:")
+    delimited!(tag!("Scenario Outline:"), take_until!("\n"), char!('\n'))
 );
 
-named!(tags_parser<&str, &str>,
-    tag_s!("@")
+named!(tag_feature_parser<&str, (Vec<&str>, &str)>,
+    alt!(
+        many_till!(
+            delimited!(tag!("@"), take_until!("\n"), char!('\n')),
+            delimited!(tag!("Feature:"), take_until!("\n"), char!('\n'))
+    ) | many_till!(
+            delimited!(tag!("Feature:"), take_until!("\n"), char!('\n')),
+            tag!("\n")
+        )
+    )
+);
+
+named!(given_parser<&str, &str>,
+    delimited!(tag!("Given"), take_until!("\n"), char!('\n'))
 );
 
 named!(when_parser<&str, &str>,
-    tag_s!("When")
+    delimited!(tag!("When"), is_not!("\n"), char!('\n'))
 );
 
 named!(then_parser<&str, &str>,
-    alt!(tag_s!("Then") | tag_s!("And"))
+    delimited!(alt!(tag!("Then") | tag!("And")), take_until!("\n"), char!('\n'))
 );
 
-named!(parameter_parser<&str, &str>,
-    delimited!(
-        char!('<'),
-        is_not!(">"),
-        char!('>')
-    )
-);
 
-named!(
-    build_outline <&str,ScenarioOutline>,do_parse!(
-        scenario_name: scenario_outline_parser >>
-        tags: tags_parser >>
-        when: when_parser >>
-        then: then_parser >>
-        (ScenarioOutline {
-            scenario: scenario_name.to_string(),
-            tags: tags.to_string(),
-            when: when.to_string(),
-            then: then.to_string()
-        })
-    )
-);
+/// Note: Not working with whitespace right now.  You have to trim.  Example code of it working:
+/// ```
+/// let filename = "foo.feature";
+/// let file = File::open(filename).unwrap();
+/// let buffered = BufReader::new(file);
+/// let lines = buffered.lines();
+///
+/// let mut file_str = "".to_string();
+/// for l in lines {
+///     file_str = file_str +  l.unwrap().trim() + "\n"
+/// }
+/// let scenario = build_outline(&file_str).unwrap();
+/// println!("Scenario {:?}", scenario);
+///```
+fn build_outline(i: &str) -> IResult<&str, ScenarioOutline> {
+    let (input, (tags_feature, scenario, given, when, then)) = tuple((
+        tag_feature_parser,
+        scenario_outline_parser,
+        given_parser,
+        when_parser,
+        then_parser,
+    ))(i)?;
+
+    Ok((
+        input,
+        ScenarioOutline {
+            scenario: scenario.trim().to_string(),
+            tags: tags_feature.0.iter().map(|&x| x.to_string()).collect(),
+            feature: tags_feature.1.trim().to_string(), //tags_feature.0,
+            given: given.trim().to_string(),
+            when: when.trim().to_string(),
+            then: then.trim().to_string(),
+        },
+    ))
+}
 
 #[cfg(test)]
 mod tests {
-    use super::tags_parser;
+    use super::build_outline;
+    use super::given_parser;
     use super::scenario_outline_parser;
-    use super::when_parser;
+    use super::tag_feature_parser;
     use super::then_parser;
-    use super::parameter_parser;
-
+    use super::when_parser;
 
     #[test]
     fn test_tag_parser() {
-        assert_eq!(
-            tags_parser("@foo"),
-            Ok(("foo", "@"))
-        )
-    }
+        let mut test_vec = Vec::new();
+        test_vec.push("foo");
+        test_vec.push("bar");
 
-    #[test]
-    fn test_tag_parser_remaining_values() {
-        assert_eq!(tags_parser("@foo").unwrap().0, "foo")
-    }
-
-    #[test]
-    fn test_scenario_outline_parser() {
         assert_eq!(
-            scenario_outline_parser("Scenario Outline: This is a test scenario"),
-            Ok((" This is a test scenario", "Scenario Outline:"))
-        )
-    }
-
-    #[test]
-    fn test_scenario_outline_parser_remaining_values() {
-        assert_eq!(
-            scenario_outline_parser("Scenario Outline: This is a test scenario").unwrap().0,
-            " This is a test scenario"
-        )
-    }
-
-    #[test]
-    fn test_when_parser() {
-        assert_eq!(
-            when_parser("When I do a funky step"),
-            Ok((" I do a funky step", "When"))
-        )
-    }
-
-    #[test]
-    fn test_when_parser_remaining_values() {
-        assert_eq!(
-            when_parser("When I do a funky step").unwrap().0,
-            " I do a funky step"
-        )
-    }
-
-    #[test]
-    fn test_then_parser_with_then() {
-        assert_eq!(
-            then_parser("Then I am like wtf"),
-            Ok((" I am like wtf", "Then"))
-        )
-    }
-
-    #[test]
-    fn test_then_parser_with_then_with_remaining_values() {
-        assert_eq!(
-            then_parser("Then I am like wtf").unwrap().0,
-            " I am like wtf"
-        )
-    }
-
-    #[test]
-    fn test_then_parser_with_and() {
-        assert_eq!(
-            then_parser("And I look at myself"),
-            Ok((" I look at myself", "And"))
-        )
-    }
-
-    #[test]
-    fn test_then_parser_with_and_remaining_values() {
-        assert_eq!(
-            then_parser("And I look at myself").unwrap().0,
-            " I look at myself"
-        )
-    }
-
-    #[test]
-    fn test_parameter_parser() {
-        assert_eq!(
-            parameter_parser("<spaghetti.jpeg>"),
-            Ok(("", "spaghetti.jpeg"))
-        )
-    }
-
-    #[test]
-    fn test_parameter_parser_remaining_values() {
-        assert_eq!(
-            parameter_parser("<spaghetti.jpeg>").unwrap().1,
-            "spaghetti.jpeg"
+            tag_feature_parser("@foo\n@bar\nFeature: This is my test feature\n").unwrap(),
+            ("", (test_vec, " This is my test feature")),
         )
     }
 }
-
-
